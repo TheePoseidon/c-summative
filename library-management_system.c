b721ef0105286bb09acf03bbd09559b4f6dbec3d
TheePoseidon <j.ngugigiat@alustudent.com>
2026-07-28 23:36:03 +0200

    Constants

---
+++ library-management_system.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype>
+
+#define TITLE_LEN     100
+#define AUTHOR_LEN    100
+#define CATEGORY_LEN  50
+#define INITIAL_CAP   4
+#define DATA_FILE     "library.dat"
